8d2ad17512fcbd43cf1ae9a9ba32bfbb7d35b965
Lakshman Kambam <klakshman318@gmail.com>
2025-03-19 01:01:05 +0530

    Update send_message.py

--- send_message.py
+++ send_message.py
@@ -1,9 +1,11 @@
 import pywhatkit
+import os
 
+phone_number = os.environ.get("PHONE_NUMBER")
 message = "Hi 👋 from GitHub Actions and pywhatkit!"
 
 pywhatkit.sendwhatmsg_instantly(
-    phone_no="+919XXXXXXXXX",  # replace with actual phone number
+    phone_no=phone_number,  # replace with actual phone number "+919xxxxxxxxx"
     message=message,
     wait_time=10,  # seconds before sending after browser opens
     tab_close=True  # close tab after sending
